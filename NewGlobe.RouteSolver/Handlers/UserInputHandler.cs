185549baf7383b66bcb19705b0a4fc8d38fec4e0
emilmirzazada <emildev09@gmail.com>
2023-09-21 03:08:28 +0200

    Updated parser

--- NewGlobe.RouteSolver/Handlers/UserInputHandler.cs
+++ NewGlobe.RouteSolver/Handlers/UserInputHandler.cs
@@ -28,15 +28,20 @@ Guide: A-> start point, B-> end point, 5-> distance between them");
             List<string> routes = input.Replace(" ", string.Empty).Split(",").ToList();
             foreach (var route in routes)
             {
-                if (route.Length != 3)
+                if (route.Length < 3)
                 {
-                    Console.WriteLine("\nERROR: Invalid input, each route should be 3 characters (e.g., \"AB5\")");
+                    Console.WriteLine("\nERROR: Invalid input, each route should be at least 3 characters (e.g., \"AB5\")");
                     return null;
                 }
 
-                if (!int.TryParse(route.AsSpan(2), out _))
+                if (!int.TryParse(route.AsSpan(2), out int distance))
                 {
                     Console.WriteLine("\nERROR: Invalid distance, couldn't parse as integer");
+                    return null;
+                }
+                if (distance <= 0)
+                {
+                    Console.WriteLine("\nERROR: Invalid distance, should be positive integer");
                     return null;
                 }
             }
